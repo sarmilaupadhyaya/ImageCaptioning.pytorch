c94a6958a800605edc5461c41c9cf972780ec92e
Sharmila Upadhyaya <sarmilaupadhyaya@outlook.com>
2021-05-26 04:28:10 +0200

    adjust for inference

--- tools/eval.py
+++ tools/eval.py
@@ -20,7 +20,11 @@ import captioning.modules.losses as losses
 import torch
 
 
-def evaluation(model='',cnn_model='resnet101', infos_path='', only_lang_eval=0, force=0,device="cpu"):
+class Namespace:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+def evaluation(model='',cnn_model='resnet101',image_folder="", infos_path='', only_lang_eval=0, force=0,device="cpu"):
     # Input arguments and options
     parser = argparse.ArgumentParser()
     # Input paths
@@ -36,41 +40,41 @@ def evaluation(model='',cnn_model='resnet101', infos_path='', only_lang_eval=0, 
     #            help='force to evaluate no matter if there are results available')
     #parser.add_argument('--device', type=str, default='cuda',
     #            help='cpu or cuda')
-    #opts.add_eval_options(parser)
-    #opts.add_diversity_opts(parser)
-    #opt = parser.parse_args()
+    function_map = {"model":model, "cnn_model":cnn_model, "infos_path":infos_path, "only_lang_eval":only_lang_eval, "force":force, "device":device, "image_folder":image_folder}
+    function_map = opts.add_eval_options(function_map, parser)
+    function_map= opts.add_diversity_opts(function_map, parser)
+    opt = Namespace(**function_map)
 
     # Load infos
-    with open(infos_path, 'rb') as f:
+    with open(opt.infos_path, 'rb') as f:
         infos = utils.pickle_load(f)
-
     # override and collect parameters
     replace = ['input_fc_dir', 'input_att_dir', 'input_box_dir', 'input_label_h5', 'input_json', 'batch_size', 'id']
     ignore = ['start_from']
 
     for k in vars(infos['opt']).keys():
         if k in replace:
-        setattr(opt, k, getattr(opt, k) or getattr(infos['opt'], k, ''))
+            setattr(opt, k, getattr(opt, k) or getattr(infos['opt'], k, ''))
         elif k not in ignore:
-        if not k in vars(opt):
-            vars(opt).update({k: vars(infos['opt'])[k]}) # copy over options from model
+            if not k in vars(opt):
+                vars(opt).update({k: vars(infos['opt'])[k]}) # copy over options from model
 
     vocab = infos['vocab'] # ix -> word mapping
 
     pred_fn = os.path.join('eval_results/', '.saved_pred_'+ opt.id + '_' + opt.split + '.pth')
     result_fn = os.path.join('eval_results/', opt.id + '_' + opt.split + '.json')
 
-    if only_lang_eval == 1 or (not force and os.path.isfile(pred_fn)): 
+    if only_lang_eval == 1 or (not opt.force and os.path.isfile(pred_fn)): 
         # if results existed, then skip, unless force is on
-        if not force:
-        try:
-            if os.path.isfile(result_fn):
-                print(result_fn)
-                json.load(open(result_fn, 'r'))
-                print('already evaluated')
-                os._exit(0)
-        except:
-            pass
+        if not opt.force:
+            try:
+                if os.path.isfile(result_fn):
+                    print(result_fn)
+                    json.load(open(result_fn, 'r'))
+                    print('already evaluated')
+                    os._exit(0)
+            except:
+                pass
 
         predictions, n_predictions = torch.load(pred_fn)
         lang_stats = eval_utils.language_eval(opt.input_json, predictions, n_predictions, vars(opt), opt.split)
@@ -78,28 +82,27 @@ def evaluation(model='',cnn_model='resnet101', infos_path='', only_lang_eval=0, 
         os._exit(0)
 
     # At this point only_lang_eval if 0
-    if not force:
+    if not opt.force:
         # Check out if 
         try:
-        # if no pred exists, then continue
-        tmp = torch.load(pred_fn)
-        # if language_eval == 1, and no pred exists, then continue
-        if opt.language_eval == 1:
-            json.load(open(result_fn, 'r'))
-        print('Result is already there')
-        os._exit(0)
+            # if no pred exists, then continue
+            tmp = torch.load(pred_fn)
+            # if language_eval == 1, and no pred exists, then continue
+            if opt.language_eval == 1:
+                json.load(open(result_fn, 'r'))
+            print('Result is already there')
+            os._exit(0)
         except:
-        pass
+            pass
 
     # Setup the model
     opt.vocab = vocab
     model = models.setup(opt)
     del opt.vocab
-    model.load_state_dict(torch.load(model, map_location='cpu'))
-    model.to(device)
+    model.load_state_dict(torch.load(opt.model, map_location='cpu'))
+    model.to(opt.device)
     model.eval()
     crit = losses.LanguageModelCriterion()
-
     # Create the Data Loader instance
     if len(opt.image_folder) == 0:
         loader = DataLoader(opt)
@@ -107,8 +110,8 @@ def evaluation(model='',cnn_model='resnet101', infos_path='', only_lang_eval=0, 
         loader = DataLoaderRaw({'folder_path': opt.image_folder, 
                             'coco_json': opt.coco_json,
                             'batch_size': opt.batch_size,
-                            'cnn_model': cnn_model,
-                            "device": device})
+                            'cnn_model': opt.cnn_model,
+                            "device": opt.device})
     # When eval using provided pretrained model, the vocab may be different from what you have in your cocotalk.json
     # So make sure to use the vocab in infos file.
     loader.dataset.ix_to_word = infos['vocab']
@@ -119,12 +122,15 @@ def evaluation(model='',cnn_model='resnet101', infos_path='', only_lang_eval=0, 
     loss, split_predictions, lang_stats = eval_utils.eval_split(model, crit, loader, 
         vars(opt))
 
-    print('loss: ', loss)
-    if lang_stats:
-        print(lang_stats)
+    #print('loss: ', loss)
+    #if lang_stats:
+    #    print(lang_stats)
 
-    if opt.dump_json == 1:
+    #if opt.dump_json == 1:
         # dump the json
-        json.dump(split_predictions, open('vis/vis.json', 'w'))
+
+    #    json.dump(split_predictions, open('imagecaptioning/vis/vis.json', 'w'))
+    return split_predictions[0]["caption"]
+
         
-evaluation(model='/output/image-captioning/model_path/model-best.pth',cnn_model='resnet101', infos_path='output/image-captioning/model_path/infos_fc_nsc-best.pkl', only_lang_eval=0, force=0,device="cpu")   
+
